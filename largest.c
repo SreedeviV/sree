7563dfae027f168ece22f9b9747624fe113f6ea9
SreedeviV <sreedevi2996@gmail.com>
2017-04-19 15:24:56 +0530

    Add files via upload

---
+++ largest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,c;
+ printf("enter the values");
+ scanf("%d %d %d",&a,&b,&c);
+ if(a>b&&a>c)
+    printf("%d",a);
+ if(b>c&&b>a)
+    printf("%d",b);
+ if(c>a&&c>b)
+    printf("%d",c);
+ getch();
+}

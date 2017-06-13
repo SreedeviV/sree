210b4c3e6a1e3a81c6234277af3be2871f520351
SreedeviV <sreedevi2996@gmail.com>
2017-06-13 12:35:33 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,22 @@
+
+public class Ascii {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        String ch;
+        int i;
+        for(i=1; i<=255; i++)
+        {
+            ch = new Character((char)i).toString();
+            System.out.print(i+ " -> " + ch + "\t");
+        }
+		
+    }
+
+	}
+
+
